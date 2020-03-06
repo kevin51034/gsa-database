e9f120494360a2e6260bc307e950ae2188e8331c
kevin51034 <48235276+kevin51034@users.noreply.github.com>
2020-03-06 10:37:59 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -7,12 +7,8 @@ const {
 var moment = require('moment');
 
 const app = express();
-
-
-// gsa-database
 const creds = require('./client_secret.json');
 const doc = new GoogleSpreadsheet('<spreadsheet key>');
-
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -30,12 +26,50 @@ async function main() {
 }
 main();
 
+
+async function onGet(req, res) {
+    sheet4 = doc.sheetsByIndex[4];
+    const rows = await sheet4.getRows({
+        offset: 2
+    });
+    console.log(rows.length);
+    // push course info into json
+    const resJson = {};
+    var key = '註冊紀錄';
+    resJson[key] = [];
+    for (let i = 0; i < rows.length; i++) {
+        var temp = {
+            變項名稱: `${rows[i].變項名稱}`,
+            身分證字號: `${rows[i].身分證字號}`,
+            學員證字號: `${rows[i].學員證字號}`,
+            姓名: `${rows[i].姓名}`,
+            中心ID: `${rows[i].中心ID}`,
+            樂齡學習中心名稱: `${rows[i].樂齡學習中心名稱}`,
+            課程ID: `${rows[i].課程ID}`,
+            課程名稱: `${rows[i].課程名稱}`,
+            課程類型: `${rows[i].課程類型}`,
+            課程主題: `${rows[i].課程主題}`,
+            拓點村里: `${rows[i].拓點村里}`,
+            課程日期: `${rows[i].課程日期}`,
+            簽到時間: `${rows[i].簽到時間}`,
+            簽退時間: `${rows[i].簽退時間}`,
+            時數: `${rows[i].時數}`,
+        };
+        resJson[key].push(temp);
+    }
+    res.json(resJson);
+
+    //res.json(studentInfo);
+}
+app.get('/api', onGet);
+
+
 // get specific sheet
 async function onGetspecific(req, res) {
     const sheetID = req.params.ID;
     // work with course sheet
     if (req.params.ID == 5) {
-        const sheet = doc.sheetsByIndex[sheetID]
+        sheet = doc.sheetsByIndex[sheetID]
         const rows = await sheet.getRows({
             offset: 1
         });
@@ -44,20 +78,17 @@ async function onGetspecific(req, res) {
         var key = '中心課程';
         resJson[key] = [];
         for (let i = 0; i < rows.length; i++) {
-            // filter out the course by time
-            if(moment().isBetween(moment(rows[i].課程開始日期, 'MMMM Do YYYY'), moment(rows[i].課程結束日期, 'MMMM Do YYYY'))) {
-                var temp = {
-                    課程名稱: `${rows[i].課程名稱}`,
-                };
-                resJson[key].push(temp);
-            }
+            var temp = {
+                課程名稱: `${rows[i].課程名稱}`,
+            };
+            resJson[key].push(temp);
         }
         res.json(resJson);
     }
 
     // work with center sheet
     if (req.params.ID == 2) {
-        const sheet = doc.sheetsByIndex[sheetID];
+        sheet = doc.sheetsByIndex[sheetID]
         const rows = await sheet.getRows({
             offset: 0
         });
@@ -78,12 +109,12 @@ async function onGetspecific(req, res) {
 }
 app.get('/api/:ID', onGetspecific);
 
-
 // get today info
 async function onGetDateHistory(req, res) {
-    const sheet = doc.sheetsByIndex[4];
+    sheet = doc.sheetsByIndex[4]
 
     const rows = await sheet.getRows();
+    //console.log(sheet.rowCount);
     // select todays history
     let todayRows = rows.filter(rows => rows.課程日期 == moment().format('MMMM Do YYYY'));
 
@@ -121,21 +152,21 @@ app.get('/api/4/today', onGetDateHistory);
 // sign in post form
 async function onPost(req, res) {
 
-    const sheet = doc.sheetsByIndex[4]
+    sheet = doc.sheetsByIndex[4]
 
     // the rest of the Info
-    const courseSheet = doc.sheetsByIndex[5]
+    courseSheet = doc.sheetsByIndex[5]
     const crows = await courseSheet.getRows();
     let courseRows = crows.filter(crows => crows.課程名稱 == req.body.課程名稱);
     //console.log(courseRows[0]);
 
-    const studentSheet = doc.sheetsByIndex[3]
+    studentSheet = doc.sheetsByIndex[3]
     const srows = await studentSheet.getRows();
     let studentRows = srows.filter(srows => srows.學員證字號 == req.body.學員證字號);
     //console.log(studentRows[0]);
 
-    req.body.中心ID = `${courseRows[0].中心ID}`;
-    req.body.課程ID = `${courseRows[0].課程ID}`;
+    req.body.中心id = `${courseRows[0].中心ID}`;
+    req.body.課程id = `${courseRows[0].課程ID}`;
     req.body.課程類型 = `${courseRows[0].課程類型}`;
     req.body.課程主題 = `${courseRows[0].課程主題}`;
     req.body.課程日期 = moment().format('MMMM Do YYYY');
@@ -147,21 +178,19 @@ async function onPost(req, res) {
 
     //console.log(req.body);
 
-    const newrow = {
+    await sheet.addRow({
         姓名: `${req.body.姓名}`,
         學員證字號: `${req.body.學員證字號}`,
         樂齡學習中心名稱: `${req.body.樂齡學習中心名稱}`,
         課程名稱: `${req.body.課程名稱}`,
         拓點村里: `${req.body.拓點村里}`,
-        中心ID: `${req.body.中心ID}`,
-        課程ID: `${req.body.課程ID}`,
+        中心ID: `${req.body.中心id}`,
+        課程ID: `${req.body.課程id}`,
         課程類型: `${req.body.課程類型}`,
         課程主題: `${req.body.課程主題}`,
         課程日期: `${req.body.課程日期}`,
         簽到時間: `${req.body.簽到時間}`,
-    }
-
-    await sheet.addRow(newrow);
+    });
     res.json({
         response: 'success'
     });
@@ -169,11 +198,16 @@ async function onPost(req, res) {
 app.post('/api', onPost);
 
 
+// sign out post form
 async function onPatch(req, res) {
-    const sheet = doc.sheetsByIndex[4]
+
+    sheet = doc.sheetsByIndex[4]
     const rows = await sheet.getRows();
+    //console.log(req.body);
 
     let thisRows = rows.filter(rows => rows.學員證字號 == req.body.學員證字號);
+
+    //console.log(thisRows);
 
     for (let i = thisRows.length - 1; i >= 0; i--) {
         // match
@@ -186,9 +220,11 @@ async function onPatch(req, res) {
 
             thisRows[i].簽退時間 = moment().format('HH:mm:ss');
 
+            //console.log(moment(thisRows[i].簽退時間 , 'HH:mm:ss') - moment(thisRows[i].簽到時間 , 'HH:mm:ss'));
             const countMs = moment(thisRows[i].簽退時間, 'HH:mm:ss') - moment(thisRows[i].簽到時間, 'HH:mm:ss');
             // have to use utc
             const countHours = moment.utc(countMs, 'x').format('HH:mm:ss');
+            //console.log(countHours);
             thisRows[i].時數 = countHours;
 
             await thisRows[i].save();
@@ -207,22 +243,25 @@ async function onPatch(req, res) {
 }
 app.patch('/api', onPatch);
 
+
 // Customize post form
 async function onPostCustomize(req, res) {
 
-    const sheet = doc.sheetsByIndex[4]
+    sheet = doc.sheetsByIndex[4]
 
     // the rest of the Info
-    const courseSheet = doc.sheetsByIndex[5]
+    courseSheet = doc.sheetsByIndex[5]
     const crows = await courseSheet.getRows();
     let courseRows = crows.filter(crows => crows.課程名稱 == req.body.課程名稱);
+    //console.log(courseRows[0]);
 
-    const studentSheet = doc.sheetsByIndex[3]
+    studentSheet = doc.sheetsByIndex[3]
     const srows = await studentSheet.getRows();
     let studentRows = srows.filter(srows => srows.學員證字號 == req.body.學員證字號);
+    //console.log(studentRows[0]);
 
-    req.body.中心ID = `${courseRows[0].中心ID}`;
-    req.body.課程ID = `${courseRows[0].課程ID}`;
+    req.body.中心id = `${courseRows[0].中心ID}`;
+    req.body.課程id = `${courseRows[0].課程ID}`;
     req.body.課程類型 = `${courseRows[0].課程類型}`;
     req.body.課程主題 = `${courseRows[0].課程主題}`;
 
@@ -234,8 +273,7 @@ async function onPostCustomize(req, res) {
     // have to use utc
     const countHours = moment.utc(countMs, 'x').format('HH:mm:ss');
     req.body.時數 = countHours;
-
-    const formatDate = moment(req.body.課程日期, 'YYYY-MM-DD').format('MMMM Do YYYY')
+    //console.log(req.body);
 
     await sheet.addRow({
         姓名: `${req.body.姓名}`,
@@ -243,11 +281,11 @@ async function onPostCustomize(req, res) {
         樂齡學習中心名稱: `${req.body.樂齡學習中心名稱}`,
         課程名稱: `${req.body.課程名稱}`,
         拓點村里: `${req.body.拓點村里}`,
-        中心ID: `${req.body.中心ID}`,
-        課程ID: `${req.body.課程ID}`,
+        中心ID: `${req.body.中心id}`,
+        課程ID: `${req.body.課程id}`,
         課程類型: `${req.body.課程類型}`,
         課程主題: `${req.body.課程主題}`,
-        課程日期: `${formatDate}`,
+        課程日期: `${req.body.課程日期}`,
         簽到時間: `${req.body.簽到時間}`,
         簽退時間: `${req.body.簽退時間}`,
         時數: `${req.body.時數}`,
@@ -259,122 +297,24 @@ async function onPostCustomize(req, res) {
 }
 app.post('/api/customize', onPostCustomize);
 
-
-// statistics
-async function onPostStatistics(req, res) {
-
-    const statisticSheet = doc.sheetsByIndex[9]
-    await statisticSheet.clear();
-    await statisticSheet.setHeaderRow([
-        '填報年度',
-        '填報月份',
-        '課程上課時段',
-        '課程ID',
-        '課程名稱',
-        '課程類型',
-        '講師姓名',
-        '辦理場次',
-        '辦理總時數',
-        '女性總參加人次',
-        '男性總參加人次',
-        '總參加人次'
-    ])
-
-    //  sign in info
-    const signInOutSheet = doc.sheetsByIndex[4]
-    const signInOutRows = await signInOutSheet.getRows({
-        offset: 2
-        //offset: 900
+async function onPatchDate(req, res) {
+    const sheet = doc.sheetsByIndex[4]
+    const rows = await sheet.getRows({
+        offset:2,
+        limit: 900
     });
 
-    // course info
-    const courseSheet = doc.sheetsByIndex[5]
-    const crows = await courseSheet.getRows({
-        offset: 2
-    });
-
-    const newStatisticRows = [];
-    const monthStatisticRows = [];
-    // map through the sign in row to get new array and patch into the statistics sheet
-    for (i = 0; i < crows.length; i++) {
-        //console.log('course iterator');
-
-        // filter the course name
-        let statisticRows = signInOutRows.filter(signInOutRows => signInOutRows.課程名稱 == `${crows[i].課程名稱}`);
-        //filter the year
-        statisticRows = statisticRows.filter(statisticRows => moment(statisticRows.課程日期, 'MMMM Do YYYY').year() == (`${parseInt(crows[i].學年度) +1912}`));
-        // filter the month
-        for (j = 0; j < 12; j++) {
-            monthStatisticRows[j] = statisticRows.filter(statisticRows => moment(statisticRows.課程日期, 'MMMM Do YYYY').month() == j);
-        }
-        let countPeople = []; // no duplicate
-        let countPeopleMan = 0; // no duplicate
-        let countPeopleWomen = 0;   // no duplicate
-        for (j = 0; j < 12; j++) {
-            if (monthStatisticRows[j].length > 0) {
-                //console.log('month iterator');
-                let manCount = 0;
-                let womenCount = 0;
-                let countSession = [];
-                for (x = 0; x < monthStatisticRows[j].length; x++) {
-                    var month = moment(monthStatisticRows[j][x].課程日期, 'MMMM Do YYYY').format("MMMM");
-                    // count the session of the course
-                    if (countSession.indexOf(monthStatisticRows[j][x].課程日期) == -1) {
-                        //console.log('push')
-                        countSession.push(monthStatisticRows[j][x].課程日期);
-                    }
-                    // count people number
-                    if (monthStatisticRows[j][x].性別 == '男') {
-                        manCount++;
-                    } else {
-                        womenCount++;
-                    }
-                    // count people number without duplicate
-                    if (countPeople.indexOf(monthStatisticRows[j][x].學員證字號) == -1) {
-                        //console.log('push')
-                        countPeople.push(monthStatisticRows[j][x].學員證字號);
-                        if(monthStatisticRows[j][x].性別 == '男') {
-                            countPeopleMan++;
-                        }else {
-                            countPeopleWomen++;
-                        }
-                    }
-                }
-                let temp = {
-                    填報年度: `${crows[i].學年度}`,
-                    填報月份: `${month}`,
-                    課程上課時段: `${crows[i].課程上課時段}`,
-                    課程ID: `${crows[i].課程ID}`,
-                    課程名稱: `${crows[i].課程名稱}`,
-                    課程類型: `${crows[i].課程類型}`,
-                    講師姓名: `${crows[i].講師姓名}`,
-                    辦理場次: `${countSession.length}`,
-                    辦理總時數: `${parseInt(crows[i].課程時數)*countSession.length}`,
-                    女性總參加人次: `${womenCount}`,
-                    男性總參加人次: `${manCount}`,
-                    總參加人次: `${womenCount + manCount}`
-                }
-                if (temp !== {}) {
-                    newStatisticRows.push(temp);
-                }
-            }
-        }
-        crows[i].女性總參加人數 = countPeopleWomen;
-        crows[i].男性總參加人數 = countPeopleMan;
-        crows[i].參加總人數 = `${countPeopleWomen + countPeopleMan}`;
-
-        await crows[i].save();
+    for(let i = 0;i<rows.length;i++) {
+        rows[i].課程日期 = moment(rows[i].課程日期, 'MM-DD-YYYY').format('MMMM Do YYYY');
+        console.log(rows[i].課程日期);
+        await rows[i].save();
     }
 
-    await statisticSheet.addRows(newStatisticRows);
-    res.json({
-        response: 'success'
-    });
 }
-app.post('/api/statistics', onPostStatistics);
+app.patch('/api/date', onPatchDate);
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 app.listen(port, function () {
     console.log(`Server listening on port ${port}!`);
